dccbfa386c45112f9e1d80f2fc6a6cec97ce9624
Igor Proskurin <igor.v.proskurin@gmail.com>
2024-11-03 00:01:04 -0700

    add trivial case w/ test

--- libc/stdlib/strtofxhr.c
+++ libc/stdlib/strtofxhr.c
@@ -1,6 +1,62 @@
+#include <ctype.h>
+#include <errno.h>
 #include <stdfix.h>
+#include "sectionname.h"
 
-short fract strtofxhr(const char* restrict nptr, char ** restrict endptr) {
+ATTRIBUTE_CLIB_SECTION
+short
+fract strtofxhr(const char* restrict nptr, char ** restrict endptr)
+{
+	union {
+		unsigned short u8;
+		fract ffx;
+	} x;
+	unsigned char c;
+	short exp = 0;
+	unsigned short base = 10;
+	unsigned char flag = 0x0;
+#define FL_NEG    0x01    /* number is negative */
+#define FL_0X     0x02    /* number has Ox prefix */
+	if (endptr)
+		*endptr = (char *)nptr;
+	/*
+	 * Skip white space and pick up leading +/- sign if any.
+	 * Allow 0x or 0X for hex, else assume decimal.
+	*/
+	do {
+		c = *nptr++;
+	} while (isspace(c));
+	
+	if (c == '-') {
+		flag = FL_NEG;
+		c = *nptr++;
+	} else if (c == '+')
+		c = *nptr++;
+	if (c == '0' && (*nptr == 'x' || *nptr == 'X')) {
+		c = nptr[1];
+		nptr += 2;
+		base = 16;
+		flag |= FL_0X;
+	}
+	x.u8 = 0;
+	/*
+	 * The Report requires that the expected form of the subject sequence
+	 * has a non-empty sequence of decimal or hexadecimal digits. For
+	 * frac, we admit any positive number of trailing zeroes.
+	*/
+	while (c == '0')
+		c = *nptr++;
+	if (isdigit(c) || (isxdigit(c) && base == 16)) {
+		if (flag & FL_NEG)
+			return SFRACT_MIN;
+		return SFRACT_MAX;
+	}
+	if (c == '.') {
+		//
+	}
+	if (c == 'P' || c == 'p') {
+		//
+	}	
 	return 0hr;
 }
 
--- tests/simulate/stdlib/strtofx-1.c
+++ tests/simulate/stdlib/strtofx-1.c
@@ -1,5 +1,28 @@
+#include <stdlib.h>
 #include <stdfix.h>
 
+static char s1[] = "  	 00000";
+static char s2[] = "0x000";
+static char s3[] = "  03";
+static char s4[] = " -0x0A";
+
 int main(void) {
-	return strtofxhr(0, 0);
+	short fract ret = 0hr;
+
+	ret = strtofxhr(s1, NULL);
+	if (ret != 0hr)
+		return __LINE__;
+	
+	ret = strtofxhr(s2, NULL);
+	if (ret != 0hr)
+		return __LINE__;
+
+	ret = strtofxhr(s3, NULL);
+	if (ret != SFRACT_MAX)
+		return __LINE__;
+
+	ret = strtofxhr(s4, NULL);
+	if (ret != SFRACT_MIN)
+		return __LINE__;
+	return 0;
 }
