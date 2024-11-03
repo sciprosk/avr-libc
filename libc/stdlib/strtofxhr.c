6b96727f5c1623f14a773c22e249a161ed7cf21c
Igor Proskurin <igor.v.proskurin@gmail.com>
2024-11-02 20:21:15 -0700

    add skeleton files for strtofxhr impl and test

---
+++ libc/stdlib/strtofxhr.c
@@ -0,0 +1,6 @@
+#include <stdfix.h>
+
+short fract strtofxhr(const char* restrict nptr, char ** restrict endptr) {
+	return 0hr;
+}
+
---
+++ tests/simulate/stdlib/strtofx-1.c
@@ -0,0 +1,5 @@
+#include <stdfix.h>
+
+int main(void) {
+	return strtofxhr(0, 0);
+}
