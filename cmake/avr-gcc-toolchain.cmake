ff457f738fc71a4bd9e1ebbff6f2e368eec622a5
Igor Proskurin <igor.v.proskurin@gmail.com>
2025-02-15 17:41:28 -0800

    pass correct flags to asm; fix file extensions

--- cmake/avr-gcc-toolchain.cmake
+++ cmake/avr-gcc-toolchain.cmake
@@ -1,9 +1,14 @@
 # the name of the target system should be "Generic" since there is no OS
-set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_NAME "Linux") # changing this to Generic shanges file extensions to "*.obj"
 set(CMAKE_SYSTEM_PROCESSOR "avr2")
 
 # specifying C compiler is enough for the entire GNU toolchain
 set(CMAKE_C_COMPILER avr-gcc)
+set(CMAKE_ASM_COMPILER avr-gcc)
+set(CMAKE_C_FLAGS_INIT "-mmcu=atmega328p")
+#set(CMAKE_ASM_FLAGS_INIT "-mmcu=atmega328p")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-mmcu=atmega328p")
 
 # target toolchain prefix
 set(CMAKE_FIND_ROOT_PATH ~/local/avr)
@@ -12,8 +17,3 @@ set(CMAKE_FIND_ROOT_PATH ~/local/avr)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-message("CMAKE_ASM_COMPILER=${CMAKE_ASM_COMPILER}")
-message("CMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
-message("CMAKE_ASM_COMPILER_ID=${CMAKE_ASM_COMPILER_ID}")
-message("CMAKE_C_COMPILER_ID=${CMAKE_C_COMPILER_ID}")
